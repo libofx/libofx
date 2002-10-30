75f1f6b17cd3854d8e055e8953fe1756e4d417b6
Benoit Grégoire <benoitg@users.sourceforge.net>
2002-10-30 02:11:06 +0000

    29/10/2001  Benoit Grégoire  <bock@step.polymtl.ca> 	* lib/ofx_preproc.cpp doc/tag_striper_test.txt: 	Proprietary tag striper is now much smarter and read routines have been fixed.  Importing a file written on a single line should now be possible. 	Added a test file for the proprietary tag striper .

--- lib/ofx_preproc.cpp
+++ lib/ofx_preproc.cpp
@@ -39,6 +39,7 @@ int ofx_proc_file(int argc, char *argv[])
   ifstream input_file;
   ofstream tmp_file;
   char buffer[READ_BUFFER_SIZE];
+  char tmp;
   string s_buffer;
   char *filenames[3];
   char tmp_filename[50];
@@ -63,27 +64,35 @@ int ofx_proc_file(int argc, char *argv[])
     else
       {
 	do{
-	  input_file.getline(buffer, sizeof(buffer));
-	  //cerr<<buffer<<"\n";
+	  input_file.getline(buffer, sizeof(buffer),'\n');
+	  //cout<<buffer<<"\n";
 	  s_buffer.assign(buffer);
+	  //cout<<"input_file.gcount(): "<<input_file.gcount()<<" sizeof(buffer): "<<sizeof(buffer)<<endl;
+	  if(input_file.gcount()<(sizeof(buffer)-1))
+	    {
+	      s_buffer.append("\n");
+	    }
+	  else if( !input_file.eof()&&input_file.fail())
+	    {
+	      input_file.clear();
+	    }
 	  if(ofx_start==false&&(s_buffer.find("<OFX>")!=string::npos||s_buffer.find("<ofx>")!=string::npos)){
 	    ofx_start=true;
 	    message_out(DEBUG,"ofx_proc_file():<OFX> has been found");
 	  }
-
+	  
 	  if(ofx_start==true&&ofx_end==false){
 	    s_buffer=sanitize_proprietary_tags(s_buffer);
-	    s_buffer.append("\n");
+	    //cout<< s_buffer<<"\n";
 	    tmp_file.write(s_buffer.c_str(), s_buffer.length());
 	  }
-
+	  
 	  if(ofx_start==true&&(s_buffer.find("</OFX>")!=string::npos||s_buffer.find("</ofx>")!=string::npos)){
 	    ofx_end=true;
 	    message_out(DEBUG,"ofx_proc_file():</OFX> has been found");
 	  }
-
 	  
-	}while(!input_file.eof());
+	}while(!input_file.eof()&&!input_file.bad());
       }
     input_file.close();
     tmp_file.close();
@@ -113,67 +122,105 @@ int ofx_proc_file(int argc, char *argv[])
 /**
    This function will strip all the OFX proprietary tags and SGML comments from the SGML string passed to it
 */
+
 string sanitize_proprietary_tags(string input_string)
 {
   unsigned int i;
+  size_t input_string_size;
+  bool strip=false;
   bool tag_open=false;
-  int tag_open_idx=0;
+  int tag_open_idx=0;//Are we within < > ?
+  bool closing_tag_open=false;//Are we within </ > ?
   int orig_tag_open_idx=0;
-  bool proprietary_tag=false;
+  bool proprietary_tag=false; //Are we within a proprietary element?
   bool proprietary_closing_tag=false;
   int crop_end_idx=0;
   char buffer[READ_BUFFER_SIZE]="";
   char tagname[READ_BUFFER_SIZE]="";
+  int tagname_idx=0;
   char close_tagname[READ_BUFFER_SIZE]="";
+ 
   for(i=0;i<READ_BUFFER_SIZE;i++){
     buffer[i]=0;
     tagname[i]=0;
     close_tagname[i]=0;
   }
-
-  for(i=0;i<=input_string.size();i++){
+  
+  input_string_size=input_string.size();
+  
+  for(i=0;i<=input_string_size;i++){
     if(input_string.c_str()[i]=='<'){
       tag_open=true;
       tag_open_idx=i;
-      if(proprietary_tag==true){
-        strncpy(close_tagname,input_string.c_str(),strlen(tagname));
-        if(strcmp(tagname,close_tagname)!=0){
+      if(proprietary_tag==true&&input_string.c_str()[i+1]=='/'){
+	//We are now in a closing tag
+        closing_tag_open=true;
+	//cout<<"Comparaison: "<<tagname<<"|"<<&(input_string.c_str()[i+2])<<"|"<<strlen(tagname)<<endl;
+        if(strncmp(tagname,&(input_string.c_str()[i+2]),strlen(tagname))!=0){
           //If it is the begining of an other tag
+	  //cout<<"DIFFERENT!"<<endl;
           crop_end_idx=i-1;
+	  strip=true;
         }
         else{
           //Otherwise, it is the start of the closing tag of the proprietary tag
           proprietary_closing_tag=true;
         }
       }
+      else if(proprietary_tag==true){
+	//It is the start of a new tag, following a proprietary tag
+	crop_end_idx=i-1;
+	strip=true;
+      }
     }
     else if(input_string.c_str()[i]=='>'){
       tag_open=false;
+      closing_tag_open=false;
+      tagname[tagname_idx]=0;
+      tagname_idx=0;
       if(proprietary_closing_tag==true){
         crop_end_idx=i;
+	strip=true;
       }
     }
-    else if(tag_open==true){
+    else if(tag_open==true&&closing_tag_open==false){
       if(input_string.c_str()[i]=='.'){
         if(proprietary_tag!=true){
           orig_tag_open_idx = tag_open_idx;
           proprietary_tag=true;
         }
       }
-      tagname[i]=input_string.c_str()[i];
+      tagname[tagname_idx]=input_string.c_str()[i];
+      tagname_idx++;
     }
-  }
+    //cerr <<i<<endl;
+    if(strip==true)
+      {
+	input_string.copy(buffer,(crop_end_idx-orig_tag_open_idx)+1,orig_tag_open_idx);
+	message_out(INFO,"sanitize_proprietary_tags() (end tag or new tag) removed: "+string(buffer));
+	input_string.erase(orig_tag_open_idx,(crop_end_idx-orig_tag_open_idx)+1);
+	i=orig_tag_open_idx-1;
+	proprietary_tag=false;
+	proprietary_closing_tag=false;
+        closing_tag_open=false;
+	tag_open=false;
+	strip=false;
+      }
+
+  }//end for
   if(proprietary_tag==true){
-    message_out(DEBUG,"sanitize_proprietary_tags() original line: "+input_string);
     if(crop_end_idx==0){//no closing tag
       crop_end_idx=input_string.size()-1;
     }
     input_string.copy(buffer,(crop_end_idx-orig_tag_open_idx)+1,orig_tag_open_idx);
-    message_out(INFO,"sanitize_proprietary_tags() removed: "+string(buffer));
+    message_out(INFO,"sanitize_proprietary_tags() (end of line) removed: "+string(buffer));
     input_string.erase(orig_tag_open_idx,(crop_end_idx-orig_tag_open_idx)+1);
   }
   return input_string;
 }
+
+
+
 /**
    This function will try to find a DTD matching the requested_version and return the full path of the DTD found (or an empty string if unsuccessfull)
    *
