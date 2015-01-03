f91dad1217044f292bcb5c04d80925176c733054
Christian Stimming <christian@cstimming.de>
2015-01-03 22:14:37 +0100

    Bug#44: fix build on NetBSD
    NetBSD, like Solaris, has a 'const' argument to iconv.

--- lib/ofx_preproc.cpp
+++ lib/ofx_preproc.cpp
@@ -310,7 +310,7 @@ int ofx_proc_file(LibofxContextPtr ctx, const char * p_filename)
             size_t outbytesleft = inbytesleft * 2 - 1;
             iconv_buffer = (char*) malloc (inbytesleft * 2);
             memset(iconv_buffer, 0, inbytesleft * 2);
-#if defined(OS_WIN32) || defined(__sun)
+#if defined(OS_WIN32) || defined(__sun) || defined(__NetBSD__)
             const char * inchar = (const char *)s_buffer.c_str();
 #else
             char * inchar = (char *)s_buffer.c_str();
