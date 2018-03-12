a40fc59016992f161c4494fb43f387dad5f0d045
Benoit Grégoire <benoitg@coeus.ca>
2018-03-11 20:03:52 -0400

    Fix #15:  Allow LIBTOOLIZE env variable to allow homebrew to use glibtoolize on macOS

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-
+set -e
 echo "Running mkdir -p config"
 mkdir -p config
-echo "Running libtoolize --force"
-libtoolize --force
+${LIBTOOLIZE=libtoolize} #Allow overriding for homebrew which uses glibtoolize
+echo "Running ${LIBTOOLIZE} --force"
+${LIBTOOLIZE} --force
 echo "Running aclocal"
 aclocal -I ./m4
 echo "Running autoheader"
