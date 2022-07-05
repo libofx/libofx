eb40ff22e5a876b3c0a72237dfe51a8f901fcb91
Dawid Wróbel <me@dawidwrobel.com>
2022-07-06 00:21:33 +0200

    Add config.h.cmake stub

---
+++ config.h.cmake
@@ -0,0 +1,8 @@
+/* config.h.  Generated from config.h.cmake by cmake  */
+
+/* Name of package */
+#define PACKAGE "libofx"
+
+/* Version number of package */
+#define VERSION "@LIBOFX_VERSION_RELEASE_STRING@"
+
